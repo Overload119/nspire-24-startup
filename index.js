f5d0d097728a8bc9d476c19323282117c6f03a9e
Overload119 <budgeneration@gmail.com>
2014-11-15 15:38:38 -0500

    Add most basic API calls, begin working on search

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
 var express       = require('express');
 var elasticsearch = require('elasticsearch');
 var config        = require('./config.js');
+var bodyParser    = require('body-parser')
 var app           = express();
 var es            = new elasticsearch.Client({
                       host: 'localhost:9200',
                       log: 'trace'
                     });
 
+app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-  res.send('hello world');
+app.post('/save_user', function(req, res) {
+  var params = req.body;
+  es.index({
+    index: 'users',
+    type: 'user',
+    body: params
+  });
+  res.json({ success: true });
+});
+
+app.post('/search', function(req, res) {
+  var keywords = req.body.query;
+
+  var result = es.search({
+    index: 'users',
+    size: 30,
+    q: keywords,
+  }).then(function(body) {
+    var sourceResults = body.hits.hits.map(function(esResult) {
+      return esResult._source;
+    });
+    res.json({ success: sourceResults.length > 0, results: sourceResults });
+  });
+});
+
+app.get('/get_users', function(req, res) {
+  es.search({
+    index: 'users',
+    size: 50,
+    body: {
+
+    }
+  });
 });
 
 app.listen(config.PORT);
--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,68 @@
 (function(global) {
+
+  var ProfileCardView = Backbone.View.extend({
+    initialize: function(data) {
+      this.person = data.person;
+    },
+    render: function() {
+      template = Handlebars.compile($('#profile-card-template').html());
+      this.$el.html(template(this.person));
+      return this.$el;
+    }
+  });
+
+  var SearchResultView = Backbone.View.extend({
+    el: '.search-results',
+    events: {
+      'keyup #search': 'onSearchKeyUp'
+    },
+    initialize: function() {
+      this._searchTimer = null;
+      $('.front').fadeOut();
+      $('.hero').removeAttr('style').addClass('condense');
+      $('.how-it-works').addClass('slide-out').one('webkitTransitionEnd transitionend', function() {
+        $('.how-it-works').hide();
+      });
+      $('.search-results').addClass('slide-in');
+    },
+    onSearchKeyUp: function(evt) {
+      var c = evt.keyCode;
+      if (this._searchTimer) {
+        clearTimeout(this._searchTimer);
+      }
+
+      if (c === 13) {
+        // If they hit enter, do an instant search.
+        this.triggerSearch();
+      } else {
+        this._searchTimer = setTimeout(this.triggerSearch.bind(this), 1000);
+      }
+    },
+    triggerSearch: function() {
+      var query = this.$el.find('#search').val();
+      debugger
+      $.ajax({
+        url: '/search',
+        type: 'POST',
+        contentType: 'application/json',
+        dataType: 'application/json',
+        data: JSON.stringify({ query: query }),
+        success: function(response) {
+          debugger
+          if (response.success) {
+            debugger
+            for (var i = 0; i < response.results.length; i++) {
+              var profileCardEl = new ProfileCardView({ person: response.results[i].linkedIn });
+              profileCardEl.appendTo( this.$el );
+            }
+          } else {
+            this.$el.find('#search-result-list').empty().append('No results found.');
+          }
+        }
+      });
+    },
+  });
+
   var lat, lng, hasLocation = false;
   var map;
   var linkedInResult;
@@ -8,7 +72,6 @@
     if ($(this).hasClass('disabled')) {
       return;
     }
-
     IN.User.authorize(onLinkedInAuth)
   });
 
@@ -34,11 +97,12 @@
     $.ajax({
       url: '/save_user',
       type: 'POST',
-      dataType: 'json',
-      data: {
+      contentType: 'application/json',
+      dataType: 'application/json',
+      data: JSON.stringify({
         linkedIn: linkedInResult,
         location: { lat: lat, lng: lng }
-      },
+      }),
       success: function(response) {
         if (response.success) {
           console.log('Information saved.');
@@ -56,23 +120,24 @@
 
   global.onLinkedInLoad = function() {
     IN.Event.on(IN, 'auth', onLinkedInAuth);
-    setupLocation();
   }
 
   global.onLinkedInAuth = function() {
-    IN.API.Profile().ids('me').fields('email-address', 'interests', 'skills', 'three-past-positions')
-      .result(setupLinkedInProfile);
-    // Once they login, show the search results view.
-    $('.front').fadeOut();
-    $('.how-it-works').addClass('slide-out').one('webkitTransitionEnd', function() {
-      $('.how-it-works').hide();
-    });
-    $('.search-results').addClass('slide-in');
+    IN.API.Profile().ids('me').fields('email-address', 'interests', 'skills',
+      'three-past-positions', 'first-name', 'headline', 'picture-url').result(setupLinkedInProfile);
+    // Once they login with LinkedIn, show result view.
+    new SearchResultView();
   }
 
   var centerGoogleMaps = function(lat, lng) {
-    map.setCenter({ lat: lat, lng: lng });
-    map.setZoom(15);
+    map.setOptions({
+      center: { lat: lat, lng: lng },
+      zoom: 15,
+      draggable: true,
+      zoomControl: true,
+      scrollwheel: true,
+      disableDoubleClickZoom: true
+    });
   }
 
   var loadGoogleMaps = function() {
@@ -90,6 +155,8 @@
     map = new google.maps.Map(document.getElementById('gmap'), mapOptions);
   }
 
-  google.maps.event.addDomListener(window, 'load', loadGoogleMaps);
-  setupLocation();
+  $(document).ready(function() {
+    google.maps.event.addDomListener(window, 'load', loadGoogleMaps);
+    setupLocation();
+  });
 })(this);
